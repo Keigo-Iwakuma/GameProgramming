4352f56f50b695f7137cfc9c1065dd3ded6ae157
Keigo-Iwakuma <sym90chn@gmail.com>
2022-06-05 10:50:37 +0900

    add Game.cpp, Main.cpp and CMakeLists.txt to build first version of executable.

---
+++ chapter01/Game.cpp
@@ -0,0 +1,74 @@
+#include "Game.hpp"
+
+
+Game::Game()
+:mWindow(nullptr)
+,mIsRunning(true)
+{
+
+}
+
+
+bool Game::Initialize()
+{
+    // Initialize SDL
+    int sdlResult = SDL_Init(SDL_INIT_VIDEO);
+    if (sdlResult != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return false;
+    }
+
+    mWindow = SDL_CreateWindow(
+        "Game Programming in C++ (Chapter 01).", // Window title
+        100,   // Top left x-coordinate of window
+        100,   // Top left y-coordinate of window
+        1024,  // Width of window
+        768,   // Height of window
+        0      // Flags (0 for no flags set)
+    );
+
+    if (!mWindow)
+    {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+
+void Game::Shutdown()
+{
+    SDL_DestroyWindow(mWindow);
+    SDL_Quit();
+}
+
+
+void Game::RunLoop()
+{
+    while (mIsRunning)
+    {
+        ProcessInput();
+        UpdateGame();
+        GenerateOutput();
+    }
+}
+
+
+void Game::ProcessInput()
+{
+
+}
+
+
+void Game::UpdateGame()
+{
+
+}
+
+
+void Game::GenerateOutput()
+{
+
+}
