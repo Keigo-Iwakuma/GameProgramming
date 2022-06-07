00376eeec5072b032ec4e745feff65a9f5f74f5b
Keigo-Iwakuma <sym90chn@gmail.com>
2022-06-07 23:58:32 +0900

    update chapter02/Game.hpp.

--- chapter02/Game.hpp
+++ chapter02/Game.hpp
@@ -1,2 +1,15 @@
 #pragma once
 
+#include <SDL2/SDL.h>
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+
+class Game
+{
+public:
+    Game();
+    bool Initialize();
+    void RunLoop();
+};
