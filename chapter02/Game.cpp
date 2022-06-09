2404fef3eb5548f5f68b6cd0934a773495cf04d5
Keigo-Iwakuma <sym90chn@gmail.com>
2022-06-09 23:48:09 +0900

    add Game::RunLoop, ProcessInput definition.

--- chapter02/Game.cpp
+++ chapter02/Game.cpp
@@ -15,3 +15,73 @@ Game::Game()
 {
 
 }
+
+bool Game::Initialize()
+{
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return false;
+    }
+
+    mWindow = SDL_CreateWindow("Game Programming in C++ (chapter02)", 100, 100, 1024, 768, 0);
+    if (!mWindow)
+    {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+        return false;
+    }
+
+    mRenderer = SDL_CreateRenderer(mWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!mRenderer)
+    {
+        SDL_Log("Failed to create renderer: %s", SDL_GetError());
+        return false;
+    }
+
+    if (IMG_Init(IMG_INIT_PNG) == 0)
+    {
+        SDL_Log("Unable to initialize SDL_image: %s", SDL_GetError());
+        return false;
+    }
+
+    LoadData();
+
+    mTicksCount = SDL_GetTicks();
+
+    return true;
+}
+
+
+void Game::RunLoop()
+{
+    while(mIsRunning)
+    {
+        ProcessInput();
+        UpdateGame();
+        GenerateOutput();
+    }
+}
+
+
+void Game::ProcessInput()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                mIsRunning = false;
+                break;
+        }
+    }
+
+    const Uint8* state = SDL_GetKeyboardState(NULL);
+    if (state[SDL_SCANCODE_ESCAPE])
+    {
+        mIsRunning = false;
+    }
+
+    // Process ship input
+    mShip->ProcessKeyboard(state);
+}
