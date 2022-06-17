e208e378b2ae8e077532d4d68bf44d9c2f8bf60a
Keigo-Iwakuma <sym90chn@gmail.com>
2022-06-17 23:58:40 +0900

    add destructor of Actor.

--- chapter02/Actor.cpp
+++ chapter02/Actor.cpp
@@ -7,6 +7,21 @@
 Actor::Actor(Game* game)
     :mState(EActive)
     ,mPosition(Vector2::Zero)
+    ,mScale(1.0f)
+    ,mRotation(0.0f)
+    ,mGame(game)
 {
     mGame->AddActor(this);
+}
+
+
+Actor::~Actor()
+{
+    mGame->RemoveActor(this);
+    // Need to delete components
+    // Because ~Component calls RemoveComponent, need a different style loop
+    while (!mComponents.empty())
+    {
+        delete mComponents.back();
+    }
 }
