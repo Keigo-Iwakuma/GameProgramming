b9674841d085e7c626a5e3cb87c266f293dfdc0d
Keigo-Iwakuma <sym90chn@gmail.com>
2022-06-30 23:55:39 +0900

    update Matrix4 in Math.hpp.

--- chapter02/Math.hpp
+++ chapter02/Math.hpp
@@ -677,5 +677,117 @@ public:
     // Invert the matrix - super slow
     void Invert();
 
+    // Get the translation component of the matrix
+    Vector3 GetTranslation() const
+    {
+        return Vector3(mat[3][0], mat[3][1], mat[3][2]);
+    }
+
+    // Get the X axis of the matrix (forward)
+    Vector3 GetXAxis() const
+    {
+        return Vector3::Normalize(Vector3(mat[0][0], mat[0][1], mat[0][2]));
+    }
+
+    // Get the Y axis of the matrix (left)
+    Vector3 GetYAxis() const
+    {
+        return Vector3::Normalize(Vector3(mat[1][0], mat[1][1], mat[1][2]));
+    }
+
+    // Get the Z axis of the matrix (up)
+    Vector3 GetZAxis() const
+    {
+        return Vector3::Normalize(Vector3(mat[2][0], mat[2][1], mat[2][2]));
+    }
+
+    // Extract the scale component from the matrix
+    Vector3 GetScale() const
+    {
+        Vector3 retVal;
+        retVal.x = Vector3(mat[0][0], mat[0][1], mat[0][2]).Length();
+        retVal.y = Vector3(mat[1][0], mat[1][1], mat[1][2]).Length();
+        retVal.z = Vector3(mat[2][0], mat[2][1], mat[2][2]).Length();
+        return retVal;
+    }
+
+    // Create a scale matrix with x, y, and z scales
+    static Matrix4 CreateScale(float xScale, float yScale, float zScale)
+    {
+        float temp[4][4] = 
+        {
+            { xScale, 0.0f, 0.0f, 0.0f },
+            { 0.0f, yScale, 0.0f, 0.0f },
+            { 0.0f, 0.0f, zScale, 0.0f },
+            { 0.0f, 0.0f, 0.0f, 1.0f },
+        };
+        return Matrix4(temp);
+    }
+
+    static Matrix4 CreateScale(const Vector3& scaleVector)
+    {
+        return CreateScale(scaleVector.x, scaleVector.y, scaleVector.z);
+    }
+
+    // Create a scale matrix with a uniform factor
+    static Matrix4 CreateScale(float scale)
+    {
+        return CreateScale(scale, scale, scale);
+    }
+
+    // Rotation about x-axis
+    static Matrix4 CreateRotationX(float theta)
+    {
+        float temp[4][4] = 
+        {
+            { 1.0f, 0.0f, 0.0f, 0.0f },
+            { 0.0f, Math::Cos(theta), Math::Sin(theta), 0.0f },
+            { 0.0f, -Math::Sin(theta), Math::Cos(theta), 0.0f },
+            { 0.0f, 0.0f, 0.0f, 1.0f },
+        };
+        return Matrix4(temp);
+    }
+
+    // Rotation about y-axis
+    static Matrix4 CreateRotationY(float theta)
+    {
+        float temp[4][4] = 
+        {
+            { Math::Cos(theta), 0.0f, -Math::Sin(theta), 0.0f },
+            { 0.0f, 1.0f, 0.0f, 0.0f },
+            { Math::Sin(theta), 0.0f, Math::Cos(theta), 0.0f },
+            { 0.0f, 0.0f, 0.0f, 1.0f },
+        };
+        return Matrix4(temp);
+    }
+
+    // Rotation about z-axis
+    static Matrix4 CreateRotationZ(float theta)
+    {
+        float temp[4][4] = 
+        {
+            { Math::Cos(theta), Math::Sin(theta), 0.0f, 0.0f },
+            { -Math::Sin(theta), Math::Cos(theta), 0.0f, 0.0f },
+            { 0.0f, 0.0f, 1.0f, 0.0f },
+            { 0.0f, 0.0f, 0.0f, 1.0f },
+        };
+        return Matrix4(temp);
+    }
+
+    // Create a rotation matrix from a quaternion
+    static Matrix4 CreateFromQuaternion(const class Quaternion& q);
+
+    static Matrix4 CreateTranslation(const Vector3& trans)
+    {
+        float temp[4][4] = 
+        {
+            { 1.0f, 0.0f, 0.0f, 0.0f },
+            { 0.0f, 1.0f, 0.0f, 0.0f },
+            { 0.0f, 0.0f, 1.0f, 0.0f },
+            { trans.x, trans.y, trans.z, 1.0f },
+        };
+        return Matrix4(temp);
+    }
+
     static const Matrix4 Identity;
 };
