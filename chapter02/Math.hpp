9acfec98266f5361bc134876088f3d4d40a4fd9c
Keigo-Iwakuma <sym90chn@gmail.com>
2022-07-04 23:29:56 +0900

    complete Math.hpp.

--- chapter02/Math.hpp
+++ chapter02/Math.hpp
@@ -939,5 +939,88 @@ public:
         return retVal;
     }
 
+    static float Dot(const Quaternion a, const Quaternion b)
+    {
+        return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
+    }
+
+    // Spherical Linear Interpolation
+    static Quaternion Slerp(const Quaternion& a, const Quaternion& b, float f)
+    {
+        float rawCosm = Quaternion::Dot(a, b);
+
+        float cosom = -rawCosm;
+        if (rawCosm >= 0.0f)
+        {
+            cosom = rawCosm;
+        }
+
+        float scale0, scale1;
+
+        if (cosom < 0.9999f)
+        {
+            const float omega = Math::Acos(cosom);
+            const float invSin = 1.0f / Math::Sin(omega);
+            scale0 = Math::Sin((1.0f - f) * omega) * invSin;
+            scale1 = Math::Sin(f * omega) * invSin;
+        }
+        else
+        {
+            // Use linear interpolation if the quaternions
+            // are collinear
+            scale0 = 1.0f - f;
+            scale1 = f;
+        }
+
+        if (rawCosm < 0.0f)
+        {
+            scale1 = -scale1;
+        }
+
+        Quaternion retVal;
+        retVal.x = scale0 * a.x + scale1 * b.x;
+        retVal.y = scale0 * a.y + scale1 * b.y;
+        retVal.z = scale0 * a.z + scale1 * b.z;
+        retVal.w = scale0 * a.w + scale1 * b.w;
+        retVal.Normalize();
+        return retVal;
+    }
+
+    // Concatenate
+    // Rotate by q FOLLOWED BY p
+    static Quaternion Concatenate(const Quaternion& q, const Quaternion& p)
+    {
+        Quaternion retVal;
+
+        // Vector component is:
+        // ps * qv + qs * pv + pv x qv
+        Vector3 qv(q.x, q.y, q.z);
+        Vector3 pv(p.x, p.y, p.z);
+        Vector3 newVec = p.w * qv + q.w * pv + Vector3::Cross(pv, qv);
+        retVal.x = newVec.x;
+        retVal.y = newVec.y;
+        retVal.z = newVec.z;
+
+        // Scalar component is;
+        // ps * qs - pv * qv
+        retVal.w = p.w * q.w - Vector3::Dot(pv, qv);
+
+        return retVal;
+    }
+
     static const Quaternion Identity;
 };
+
+namespace Color
+{
+    static const Vector3 Black(0.0f, 0.0f, 0.0f);
+    static const Vector3 White(1.0f, 1.0f, 1.0f);
+    static const Vector3 Red(1.0f, 0.0f, 0.0f);
+    static const Vector3 Green(0.0f, 1.0f, 0.0f);
+    static const Vector3 Blue(0.0f, 0.0f, 1.0f);
+    static const Vector3 Yellow(1.0f, 1.0f, 0.0f);
+    static const Vector3 LightYellow(1.0f, 1.0f, 0.88f);
+    static const Vector3 LightBlue(0.68f, 0.85f, 0.9f);
+    static const Vector3 LightPink(1.0f, 0.71f, 0.76f);
+    static const Vector3 LightGreen(0.56f, 0.93f, 0.56f);
+}
