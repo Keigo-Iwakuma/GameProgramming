dce2cdd55ca727c09e87a78da7e11747e6b769d0
Keigo-Iwakuma <sym90chn@gmail.com>
2022-07-05 23:54:45 +0900

    update Math.cpp.

---
+++ chapter02/Math.cpp
@@ -0,0 +1,36 @@
+#include "Math.hpp"
+
+const Vector2 Vector2::Zero(0.0f, 0.0f);
+const Vector2 Vector2::UnitX(1.0f, 0.0f);
+const Vector2 Vector2::UnitY(0.0f, 1.0f);
+const Vector2 Vector2::NegUnitX(-1.0f, 0.0f);
+const Vector2 Vector2::NegUnitY(0.0f, -1.0f);
+
+const Vector3 Vector3::Zero(0.0f, 0.0f, 0.0f);
+const Vector3 Vector3::UnitX(1.0f, 0.0f, 0.0f);
+const Vector3 Vector3::UnitY(0.0f, 1.0f, 0.0f);
+const Vector3 Vector3::UnitZ(0.0f, 0.0f, 1.0f);
+const Vector3 Vector3::NegUnitX(-1.0f, 0.0f, 0.0f);
+const Vector3 Vector3::NegUnitY(0.0f, -1.0f, 0.0f);
+const Vector3 Vector3::NegUnitZ(0.0f, 0.0f, -1.0f);
+const Vector3 Vector3::Infinity(Math::Infinity, Math::Infinity, Math::Infinity);
+const Vector3 Vector3::NegInfinity(Math::NegInfinity, Math::NegInfinity, Math::NegInfinity);
+
+static float m3Ident[3][3] = 
+{
+    { 1.0f, 0.0f, 0.0f },
+    { 0.0f, 1.0f, 0.0f },
+    { 0.0f, 0.0f, 1.0f },
+};
+
+const Matrix3 Matrix3::Identity(m3Ident);
+
+static float M4Ident[4][4] = 
+{
+    { 1.0f , 0.0f , 0.0f, 0.0f },
+    { 0.0f , 1.0f , 0.0f, 0.0f },
+    { 0.0f , 0.0f , 1.0f, 0.0f },
+    { 0.0f , 0.0f , 0.0f, 1.0f },
+};
+
+const Matrix4 Matrix4::Identity(M4Ident);
