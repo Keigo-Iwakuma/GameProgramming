c46c8dab069114880aa4c07f937501306cfd0cda
Keigo-Iwakuma <sym90chn@gmail.com>
2022-07-06 23:57:22 +0900

    update Math.cpp.

--- chapter02/Math.cpp
+++ chapter02/Math.cpp
@@ -34,3 +34,57 @@ static float M4Ident[4][4] =
 };
 
 const Matrix4 Matrix4::Identity(M4Ident);
+
+const Quaternion Quaternion::Identity(0.0f, 0.0f, 0.0f, 1.0f);
+
+Vector2 Vector2::Transform(const Vector2& vec, const Matrix3& mat, float w /*= 1.0f*/)
+{
+    Vector2 retVal;
+    retVal.x = vec.x * mat.mat[0][0] + vec.y * mat.mat[1][0] + w * mat.mat[2][0];
+    retVal.y = vec.x * mat.mat[0][1] + vec.y * mat.mat[1][1] + w * mat.mat[2][1];
+    // ignore w since we aren't returning a new value for it ...
+    return retVal;
+}
+
+Vector3 Vector3::Transform(const Vector3& vec, const Matrix4& mat, float w /*= 1.0f*/)
+{
+    Vector3 retVal;
+    retVal.x = vec.x * mat.mat[0][0] + vec.y * mat.mat[1][0] +
+        vec.z * mat.mat[2][0] + w * mat.mat[3][0];
+    retVal.y = vec.x * mat.mat[0][1] + vec.y * mat.mat[1][1] +
+        vec.z * mat.mat[2][1] + w * mat.mat[3][1];
+    retVal.z = vec.x * mat.mat[0][2] + vec.y * mat.mat[1][2] +
+        vec.z * mat.mat[2][2] + w * mat.mat[3][2];
+    // ignore w since we aren't returning a new value for it ...
+    return retVal;
+}
+
+// This will transform the vector and renormalize the w component
+Vector3 Vector3::TransformWithPerspDiv(const Vector3& vec, const Matrix4& mat, float w /*= 1.0f*/)
+{
+    Vector3 retVal;
+    retVal.x = vec.x * mat.mat[0][0] + vec.y * mat.mat[1][0] +
+        vec.z * mat.mat[2][0] + w * mat.mat[3][0];
+    retVal.y = vec.x * mat.mat[0][1] + vec.y * mat.mat[1][1] +
+        vec.z * mat.mat[2][1] + w * mat.mat[3][1];
+    retVal.z = vec.x * mat.mat[0][2] + vec.y * mat.mat[1][2] +
+        vec.z * mat.mat[2][2] + w * mat.mat[3][2];
+    float transformedW = vec.x * mat.mat[0][3] + vec.y * mat.mat[1][3] +
+        vec.z * mat.mat[2][3] + w * mat.mat[3][3];
+    if (!Math::NearZero(Math::Abs(transformedW)))
+    {
+        transformedW = 1.0f / transformedW;
+        retVal *= transformedW;
+    }
+    return retVal;
+}
+
+// Transform a Vector3 by a quaternion
+Vector3 Vector3::Transform(const Vector3& v, const Quaternion& q)
+{
+    // v + 2.0 * cross(q.xyz, cross(q.xyz, v) + q.w * v)
+    Vector3 qv(q.x, q.y, q.z);
+    Vector3 retVal = v;
+    retVal += 2.0f * Vector3::Cross(qv, Vector3::Cross(qv, v) + q.w * v);
+    return retVal;
+}
