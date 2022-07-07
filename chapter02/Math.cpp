46c85962bb415e6b8263bee181f4c0e5e57f2576
Keigo-Iwakuma <sym90chn@gmail.com>
2022-07-07 23:56:19 +0900

    add Matrix4::Invert in Math.cpp.

--- chapter02/Math.cpp
+++ chapter02/Math.cpp
@@ -88,3 +88,38 @@ Vector3 Vector3::Transform(const Vector3& v, const Quaternion& q)
     retVal += 2.0f * Vector3::Cross(qv, Vector3::Cross(qv, v) + q.w * v);
     return retVal;
 }
+
+void Matrix4::Invert()
+{
+    // Thanks slow math
+    // This is a really janky way to unroll everything ...
+    float tmp[12];
+    float src[16];
+    float dst[16];
+    float det;
+
+    // Transpose matrix
+    // row 1 to col 1
+    src[0] = mat[0][0];
+    src[4] = mat[0][1];
+    src[8] = mat[0][2];
+    src[12] = mat[0][3];
+
+    // row 2 to col 2
+    src[1] = mat[1][0];
+    src[5] = mat[1][1];
+    src[9] = mat[1][2];
+    src[13] = mat[1][3];
+
+    // row 3 to col 3
+    src[2] = mat[2][0];
+    src[6] = mat[2][1];
+    src[10] = mat[2][2];
+    src[14] = mat[2][3];
+
+    // row 4 to col 4
+    src[3] = mat[3][0];
+    src[7] = mat[3][1];
+    src[11] = mat[3][2];
+    src[15] = mat[3][3];
+}
